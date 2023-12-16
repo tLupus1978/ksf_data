bd95e5441c7b8da5b9b2f897ea18c77aaafdf4f6
Thomas Wolf <t.wolf@cid.com>
2023-12-16 17:33:19 +0000

    updates

--- sensor.py
+++ sensor.py
@@ -1,16 +1,20 @@
-"""
-Portainer Status Support
+"""Portainer Status Support.
 
 Original Author:  Jim Thompson
 
 Description:
   Configuration Instructions are on GitHub.
-
 """
 
+from datetime import datetime, timedelta
+from html.parser import HTMLParser
 import logging
+from typing import List  # noqa: UP035
+
+import pyquery
+from pyquery import PyQuery as pq
 import requests
-from datetime import timedelta, datetime
+
 from homeassistant.helpers.entity import Entity
 from homeassistant.util import Throttle
 
@@ -18,40 +22,45 @@ _LOGGER = logging.getLogger(__name__)
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=10)
 
+DEFAULT_LOGIN_URL = "https://login.schulportal.hessen.de/?url=aHR0cHM6Ly9jb25uZWN0LnNjaHVscG9ydGFsLmhlc3Nlbi5kZS8=&skin=sp&i=6013"
+DEFAULT_LANDINGPAGE_URL = "https://connect.schulportal.hessen.de/"
+DEFAULT_SUBSTITUTE_URL = "https://start.schulportal.hessen.de/vertretungsplan.php"
+
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    url = config.get("url")
+    # url = config.get("url")
     username = config.get("username")
     password = config.get("password")
     name = config.get("name")
 
-    if not url or not username or not password or not name:
-        _LOGGER.error("URL, username, name or password not provided.")
-        return False
+    # if not url or not username or not password or not name:
+    #    _LOGGER.error("URL, username, name or password not provided.")
+    #    return False
 
     try:
         _LOGGER.debug(f"Loading KSF data from {url}")
-        ksf = KSFData(url, username, password)
+        ksf = ksfData(username, password)
         ksf.update()
     except Exception as e:
         _LOGGER.error(f"Failed to connect to KSF: {e}")
         return False
 
-    ksf_entity = ksfSensor(ksf, url, name, username)
+    ksf_entity = ksfSensor(ksf, name, username)
     add_entities([ksf_entity], True)
-    add_entities(
-        [
-            PortainerContainerSensor(container, ksf, url, name)
-            for container in ksf.containers
-        ],
-        True,
-    )
+
+
+#    add_entities(
+#        [
+#            PortainerContainerSensor(container, ksf, url, name)
+#            for container in ksf.containers
+#        ],
+#        True,
+#    )
 
 
 class ksfSensor(Entity):
-    def __init__(self, ksf, url, name, username):
+    def __init__(self, ksf, name, username):
         self._ksf = ksf
-        self._url = url
         self._state = None
         self._attributes = {}
         self._name = f"ksf_daten_{name}"
@@ -88,7 +97,6 @@ class ksfSensor(Entity):
             "FriendlyName": self._username,
             # "Version": self._portainer.version,
             "NumberOfSubstitutes": len(self._ksf.substitutes),
-            "url": self._url,
         }
 
     def update(self):
@@ -99,146 +107,172 @@ class ksfSensor(Entity):
         self._state = self._ksf.now
 
 
-class PortainerContainerSensor(Entity):
-    def __init__(self, container, portainer, url, server_name):
-        self._container = container
-        self._portainer = portainer
-        self._url = url
-        self._state = None
-        self._attributes = {}
-        self._name = f"portainer_{server_name}_{container['Names'][0].strip('/')}"
-
-    @property
-    def unique_id(self):
-        return self._name
-
-    #        return f"portainer_container_{self._container['Id']}"
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def state(self):
-        return self._state
-
-    @property
-    def icon(self):
-        return "mdi:docker"
-
-    @property
-    def extra_state_attributes(self):
-        attrs = {
-            "Name": self._container["Names"][0].strip("/"),
-            "Image": self._container["Image"],
-            "ContainerId": self._container["Id"],
-            "Created": datetime.fromtimestamp(self._container["Created"]).strftime(
-                "%Y%m%dT%H:%M:%S"
-            ),
-            "Status": self._container["Status"],
-            "Ports": [
-                f"{port.get('PublicPort', 'N/A')}->{port['PrivatePort']}/{port['Type']}"
-                for port in self._container["Ports"]
-                if not ":" in str(port.get("IP", ""))
-            ],
-            "url": self._url,
-            "parent_friendly_name": f"portainer_server_{self._name.split('_')[1]}",
-            "parent_instance_id": self._portainer.instance_id,
-        }
-        return attrs
-
-    def update(self):
-        # Update the container data from the PortainerData instance
-        self._container = next(
-            (c for c in self._portainer.containers if c["Id"] == self._container["Id"]),
-            None,
-        )
-
-        if self._container:
-            _LOGGER.debug(
-                f"Updating container named: {self._container['Names'][0].strip('/')} ({self._container['Image']})  from {self._url}"
-            )
-            self._state = self._container["State"]
-        else:
-            _LOGGER.error(f"Container not found: {self.unique_id}")
-
-
 class ksfData:
-    def __init__(self, url, username, password):
-        self._url = url
+    def __init__(self, username, password):
         self._username = username
         self._password = password
-        self._jwt = None
-        self.instance_id = None
-        self.version = None
+        self._substituteplan = None
         self.substitutes = []
-        self.endpoint_id = None
 
     @Throttle(MIN_TIME_BETWEEN_UPDATES)
     def update(self):
-        if not self._jwt:
-            self._jwt = self._get_jwt()
+        if not self._substituteplan:
+            self._substituteplan = self._get_substituteplan()
 
-        if self._jwt:
-            if not self.endpoint_id:
-                self.endpoint_id = self._get_first_endpoint_id()
-            self.containers = self._get_containers(self.endpoint_id)
-        else:
-            _LOGGER.error("Failed to authenticate with Portainer.")
+        # if self._substituteplan:
+        #    if not self.endpoint_id:
+        #        self.endpoint_id = self._get_first_endpoint_id()
+        #    self.containers = self._get_containers(self.endpoint_id)
+        # else:
+        #    _LOGGER.error("Failed to authenticate with Portainer.")
 
-    def _get_jwt(self):
+    def _get_substituteplan(self):
+        """Vertretungsplan getter using pyscript."""
         try:
-            response = requests.post(
-                f"{self._url}/api/auth",
-                json={"Username": self._username, "Password": self._password},
-                timeout=5,
-            )
-            response.raise_for_status()
-            return response.json().get("jwt")
-        except Exception as e:
-            _LOGGER.error(f"Failed to get JWT: {e}")
-            return None
+            # log.info(f"Vertretungsplan: got user {user} password {password}")
+            login_url = DEFAULT_LOGIN_URL
+            landing_url = DEFAULT_LANDINGPAGE_URL
+            substitute_url = DEFAULT_SUBSTITUTE_URL
+            payload = {
+                "user2": "user",
+                "user": "6013." + str(self._username),
+                "password": str(self._password),
+                "skin": "sp",
+                "timezone": "1",
+                "url": "aHR0cHM6Ly9jb25uZWN0LnNjaHVscG9ydGFsLmhlc3Nlbi5kZS8=",
+            }
+            headers = {"User-Agent": "Mozilla/5.0"}
 
-    def _get_first_endpoint_id(self):
-        try:
-            response = requests.get(
-                f"{self._url}/api/endpoints",
-                headers={"Authorization": f"Bearer {self._jwt}"},
-                timeout=5,
+            # log.info(f"Start login")
+            session = requests.Session()
+            resp = session.get(login_url, headers=headers, timeout=5)
+            # did this for first to get the cookies from the page, stored them with next line:
+            cookies = requests.utils.cookiejar_from_dict(
+                requests.utils.dict_from_cookiejar(session.cookies)
             )
-            response.raise_for_status()
-            endpoints = response.json()
-            for endpoint in endpoints:
-                if endpoint["Type"] != 4:  # Filter out edge agent endpoints
-                    return endpoint["Id"]
-            return None
-        except Exception as e:
-            _LOGGER.error(f"Failed to get endpoint ID: {e}")
-            return None
+            resp = session.post(
+                login_url, headers=headers, data=payload, cookies=cookies
+            )
+            if resp.status_code != 200:
+                retVal = "Login failed!"
+                _LOGGER.error("Login not possible")
+                return retVal, None
 
-    def _get_containers(self, endpoint_id):
-        try:
-            response = requests.get(
-                f"{self._url}/api/endpoints/{endpoint_id}/docker/containers/json?all=1",
-                headers={"Authorization": f"Bearer {self._jwt}"},
-                timeout=5,
+            cookies = requests.utils.cookiejar_from_dict(
+                requests.utils.dict_from_cookiejar(session.cookies)
             )
-            response.raise_for_status()
-            return response.json()
-        except Exception as e:
-            _LOGGER.error(f"Failed to get containers: {e}")
-            return []
+            resp = session.get(landing_url, cookies=cookies)
+            if resp.status_code != 200:
+                retVal = "Calling connect.schulportal.hessen.de not possible"
+                _LOGGER.error("Calling connect.schulportal.hessen.de not possible")
+                return retVal, None
 
-    def _get_status(self):
-        try:
-            response = requests.get(
-                f"{self._url}/api/status",
-                headers={"Authorization": f"Bearer {self._jwt}"},
-                timeout=5,
+            cookies = requests.utils.cookiejar_from_dict(
+                requests.utils.dict_from_cookiejar(session.cookies)
             )
-            response.raise_for_status()
-            status_data = response.json()
-            return status_data["InstanceID"], status_data["Version"]
+
+            resp = session.get(
+                substitute_url,
+                cookies=cookies,
+            )
+            if resp.status_code != 200:
+                retVal = "Calling https://start.schulportal.hessen.de/vertretungsplan.php not possible"
+                _LOGGER.error(
+                    "Calling https://start.schulportal.hessen.de/vertretungsplan.php not possible"
+                )
+                return retVal, None
+
+            # start processing and parsing of the page...
+            d = pq(resp.text)
+
+            dates = []
+            VertretungenDates = d(".panel-body")
+            for my_div in VertretungenDates.items():
+                dates.append(my_div("h3").text())
+
+            p = HTMLTableParser()
+            p.feed(resp.text)
+            data = p.tables
+
+            merged_data = ""
+            i = 1
+            for inner_list in data:
+                if inner_list[0][1] == "Datenschutz | Impressum":
+                    break
+                merged_data += dates[i] + "</br>\n"
+                for sublist in inner_list:
+                    merged_data += (
+                        " ".join(sublist) + "</br>\n"
+                    )  # Füge die Elemente der Unterlisten durch Tabs getrennt zusammen
+                i += 1
+
+            return merged_data, None
         except Exception as e:
-            _LOGGER.error(f"Failed to get status: {e}")
-            return None, None
+            return None, e
+
+
+class HTMLTableParser(HTMLParser):
+    """This class serves as a html table parser. It is able to parse multiple
+    tables which you feed in. You can access the result per .tables field.
+    """
+
+    def __init__(
+        self,
+        decode_html_entities: bool = False,
+        data_separator: str = " ",
+    ) -> None:
+        HTMLParser.__init__(self, convert_charrefs=decode_html_entities)
+
+        self._data_separator = data_separator
+
+        self._in_td = False
+        self._in_th = False
+        self._current_table = []
+        self._current_row = []
+        self._current_cell = []
+        self.tables = []
+        self.named_tables = {}
+        self.name = ""
+
+    def handle_starttag(self, tag: str, attrs: List) -> None:
+        """We need to remember the opening point for the content of interest.
+        The other tags (<table>, <tr>) are only handled at the closing point.
+        """
+        if tag == "table":
+            name = [a[1] for a in attrs if a[0] == "id"]
+            if len(name) > 0:
+                self.name = name[0]
+        if tag == "td":
+            self._in_td = True
+        if tag == "th":
+            self._in_th = True
+
+    def handle_data(self, data: str) -> None:
+        """This is where we save content to a cell"""
+        if self._in_td or self._in_th:
+            self._current_cell.append(data.strip())
+
+    def handle_endtag(self, tag: str) -> None:
+        """Here we exit the tags. If the closing tag is </tr>, we know that we
+        can save our currently parsed cells to the current table as a row and
+        prepare for a new row. If the closing tag is </table>, we save the
+        current table and prepare for a new one.
+        """
+        if tag == "td":
+            self._in_td = False
+        elif tag == "th":
+            self._in_th = False
+
+        if tag in ["td", "th"]:
+            final_cell = self._data_separator.join(self._current_cell).strip()
+            self._current_row.append(final_cell)
+            self._current_cell = []
+        elif tag == "tr":
+            self._current_table.append(self._current_row)
+            self._current_row = []
+        elif tag == "table":
+            self.tables.append(self._current_table)
+            if len(self.name) > 0:
+                self.named_tables[self.name] = self._current_table
+            self._current_table = []
+            self.name = ""
